dbafa27494a7585e877509a568704c0677b223b4
Gayathri-Anandhan <153351016+Gayathri-Anandhan@users.noreply.github.com>
2026-01-14 20:57:16 +0530

    Add files via upload

---
+++ InsertEmployeeJDBC.java
@@ -0,0 +1,58 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class InsertEmployeeJDBC {
+
+    public static void main(String[] args) {
+
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+            String url =
+            "jdbc:sqlserver://localhost:1433;databaseName=EmployeeDB;encrypt=true;trustServerCertificate=true";
+
+            Connection con = DriverManager.getConnection(url, "sa", "password");
+
+            String sql = "INSERT INTO EmployeeJDBC VALUES (?, ?, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ps.setInt(1, 101);
+            ps.setString(2, "Jenny");
+            ps.setInt(3, 25);
+            ps.setInt(4, 10000);
+            ps.executeUpdate();
+
+            ps.setInt(1, 102);
+            ps.setString(2, "Jacky");
+            ps.setInt(3, 30);
+            ps.setInt(4, 20000);
+            ps.executeUpdate();
+
+            ps.setInt(1, 103);
+            ps.setString(2, "Joe");
+            ps.setInt(3, 20);
+            ps.setInt(4, 40000);
+            ps.executeUpdate();
+
+            ps.setInt(1, 104);
+            ps.setString(2, "John");
+            ps.setInt(3, 40);
+            ps.setInt(4, 80000);
+            ps.executeUpdate();
+
+            ps.setInt(1, 105);
+            ps.setString(2, "Shameer");
+            ps.setInt(3, 25);
+            ps.setInt(4, 90000);
+            ps.executeUpdate();
+
+            System.out.println("Records inserted successfully");
+
+            con.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
